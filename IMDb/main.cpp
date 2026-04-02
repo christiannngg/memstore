d06247398b5ca172656a8debe6446afc93cb2258
Christian Gonzalez <christianleegonzalez9@gmail.com>
2026-04-02 16:32:01 -0700

    init commit...

---
+++ IMDb/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "server.h"
+
+int main() {
+    std::cout << "IMDb starting..." << std::endl;
+    runServer();
+    return 0;
+}
---
+++ IMDb/server.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "server.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <cstring>
+
+void runServer() {
+    std::cout << "placeholder function....\n";
+}
+
+
